80c7eecb71d57eaeb4ba73931234ef5db0a4eb43
ub <kimub1204@naver.com>
2022-11-21 19:52:37 +0900

    style: 상대경로에서 절대경로로 변경

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import eslint from "@rollup/plugin-eslint";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
