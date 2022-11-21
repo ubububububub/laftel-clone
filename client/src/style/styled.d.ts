93401a8e74f4b9022b14885598fb93854931c9e0
ub <kimub1204@naver.com>
2022-11-21 22:19:52 +0900

    style: style-component theme 변경

--- client/src/style/styled.d.ts
+++ client/src/style/styled.d.ts
@@ -2,11 +2,9 @@ import "styled-components";
 
 declare module "styled-components" {
   export interface DefaultTheme {
-    borderRadius: string;
-
     colors: {
-      main: string;
-      secondary: string;
+      white: string;
+      black: string;
     };
   }
 }
