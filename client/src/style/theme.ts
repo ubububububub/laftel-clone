fa0af19dd2ddf8addb7b34a1953d32c5e50e3416
ub <kimub1204@naver.com>
2022-11-21 22:19:52 +0900

    style: style-component theme 변경

--- client/src/style/theme.ts
+++ client/src/style/theme.ts
@@ -1,11 +1,9 @@
 import { DefaultTheme } from "styled-components";
 
 const theme: DefaultTheme = {
-  borderRadius: "5px",
-
   colors: {
-    main: "cyan",
-    secondary: "magenta",
+    white: "#fff",
+    black: "#121212",
   },
 };
 
