f442c6eafcbfe253c31d6e70e2c1a5e688fec262
ub <kimub1204@naver.com>
2022-11-22 17:19:17 +0900

    refactor: 푸터 컴포넌트 분리

--- client/src/components/Footer/styled.ts
+++ client/src/components/Footer/styled.ts
@@ -10,54 +10,11 @@ export const Footer = styled.footer`
   font-size: 1.4rem;
 `;
 
-export const Content = styled.div`
+export const Nav = styled.div`
   display: flex;
   justify-content: space-between;
 
   & a {
     color: ${({ theme: { colors } }) => colors.white};
-  }
-`;
-
-export const Texts = styled.ul`
-  gap: 3.2rem;
-  display: flex;
-  align-items: center;
-
-  & > li:last-child {
-    font-weight: 700;
-  }
-`;
-
-export const Icons = styled.ul`
-  margin-left: 1.7565rem;
-  gap: 1.6rem;
-  display: flex;
-  align-items: center;
-`;
-
-export const CLink = styled.a`
-  display: block;
-`;
-
-export const Description = styled.div`
-  color: ${({ theme: { colors } }) => colors.white};
-`;
-
-export const InfoDisplay = styled.div`
-  margin: 3.2rem 0.4rem 0.8rem;
-`;
-
-export const Toggle = styled.div``;
-
-export const InfoContent = styled.div`
-  gap: 0.8rem;
-  display: flex;
-  flex-direction: column;
-  font-size: 1.2rem;
-  font-weight: 500;
-
-  & p:first-child {
-    margin-top: 0.4rem;
   }
 `;
---
+++ client/src/components/FooterMenuList/FooterMenuList.tsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+
+import * as S from "@/components/FooterMenuList/styled";
+import { Logo } from "@/components/svgs";
+
+const texts = [
+  { name: "회사소개", src: "https://laftel.oopy.io/" },
+  { name: "고객센터", src: "https://help.laftel.net/hc/ko" },
+  {
+    name: "공지사항",
+    src: "https://help.laftel.net/hc/ko/sections/360009073293/",
+  },
+  { name: "청소년 보호 정책", src: "https://policy.laftel.net/youth/2022/" },
+  { name: "개인정보 처리방침", src: "https://policy.laftel.net/privacy/2022/" },
+];
+
+const mapedTexts = texts.map((text, index) => (
+  <li key={index}>
+    <S.MenuLink href={text.src} target='_blank' rel='noreferrer'>
+      {text.name}
+    </S.MenuLink>
+  </li>
+));
+
+export function FooterMenuList() {
+  return (
+    <S.MenuList>
+      <li>
+        <Link to='/'>
+          <Logo />
+        </Link>
+      </li>
+      {mapedTexts}
+    </S.MenuList>
+  );
+}
