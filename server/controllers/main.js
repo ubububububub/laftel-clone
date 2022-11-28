01244ba460197e5361eaeb14b2b41a00afcad041
Salmambo <zxcvsqwe@gmail.com>
2022-11-28 21:33:11 +0900

    docs: 요일별api 분리. GET /api/days

--- server/controllers/main.js
+++ server/controllers/main.js
@@ -1,20 +1,14 @@
 import { Router } from "express";
-import {
-  itemService,
-  posterService,
-  themeService,
-  viewService,
-} from "../services";
+import { posterService, themeService, viewService } from "../services";
 
 const mainController = Router();
 
 mainController.get("/", async (req, res, next) => {
   try {
     const posters = await posterService.findAll();
-    const days = await itemService.findByDays();
     const themes = await themeService.findAll();
     const hots = await viewService.getHots();
-    res.status(200).json({ posters, days, themes, hots });
+    res.status(200).json({ posters, themes, hots });
   } catch (err) {
     next(err);
   }
