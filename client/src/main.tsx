13c37ad90ee761c35b02962525857482edc22883
ub <kimub1204@naver.com>
2022-11-29 17:35:43 +0900

    feat: react-query boilerplate 작성

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,5 @@
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
@@ -8,11 +10,16 @@ import { store } from "@/store";
 import { GlobalStyle } from "@/style/globalStyle";
 import theme from "@/style/theme";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <ThemeProvider {...{ theme }}>
     <GlobalStyle />
-    <Provider {...{ store }}>
-      <RouterProvider {...{ router }} />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Provider {...{ store }}>
+        <RouterProvider {...{ router }} />
+      </Provider>
+    </QueryClientProvider>
   </ThemeProvider>,
 );
