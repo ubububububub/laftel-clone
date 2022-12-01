98a256da81c8f399bab2e08c6f2b4cdb2bdc07fc
Salmambo <zxcvsqwe@gmail.com>
2022-12-01 20:13:49 +0900

    docs: 요일별api 응답값 2차원 배열로

--- server/services/item.js
+++ server/services/item.js
@@ -14,16 +14,16 @@ class ItemService {
       updateDay: { $exists: true },
     });
     if (items.length === 0) return {};
-    const days = {
-      mon: [],
-      tue: [],
-      wed: [],
-      thu: [],
-      fri: [],
-      sat: [],
-      sun: [],
-    };
-    items.forEach((item) => days[item.updateDay].push(item));
+    const days = [[], [], [], [], [], [], []];
+    items.forEach((item) => {
+      if (item.updateDay === "mon") days[0].push(item);
+      else if (item.updateDay === "tue") days[1].push(item);
+      else if (item.updateDay === "wed") days[2].push(item);
+      else if (item.updateDay === "thu") days[3].push(item);
+      else if (item.updateDay === "fri") days[4].push(item);
+      else if (item.updateDay === "sat") days[5].push(item);
+      else if (item.updateDay === "sun") days[6].push(item);
+    });
     return days;
   }
   async findByTitle({ keyword }) {
