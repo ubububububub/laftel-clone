ed99237e2f17cd43885e627f3b78af324a1d988a
Salmambo <zxcvsqwe@gmail.com>
2022-12-06 20:54:29 +0900

    docs: 테마 설명 추가

--- server/models/schemas/theme.js
+++ server/models/schemas/theme.js
@@ -4,6 +4,7 @@ const themeSchema = new Schema(
   {
     items: [{ type: Schema.Types.ObjectId, ref: "items", required: true }],
     title: { type: String, required: true },
+    description: { type: String, required: true },
   },
   {
     collection: "themes",
