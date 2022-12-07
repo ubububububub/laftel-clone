ca2f75eb4395bf2d98eecb8e83ccc68e77eb17c1
Salmambo <zxcvsqwe@gmail.com>
2022-12-08 03:40:49 +0900

    fix: controllers 구조 변경에 따른 itemController 수정

--- server/controllers/main/item/reviews.js
+++ server/controllers/main/item/reviews.js
@@ -1,25 +1,28 @@
-import { Router } from "express";
 import { reviewService, videoService } from "../../../services";
 
-const reviewsController = Router();
-
-reviewsController.post("/", async (req, res, next) => {
-  try {
-    // params 되나
-    const stars = await reviewService.create(req.params, "userInfo", req.body);
-    await videoService.updateStars(req.params, stars);
-    res.status(201).end();
-  } catch (err) {
-    next(err);
-  }
-});
-reviewsController.get("/", async (req, res, next) => {
-  try {
-    const reviews = await reviewService.findByItem(req.params);
-    res.status(200).json(reviews);
-  } catch (err) {
-    next(err);
-  }
-});
+const reviewsController = {
+  post: async (req, res, next) => {
+    try {
+      // params 되나
+      const stars = await reviewService.create(
+        req.params,
+        "userInfo",
+        req.body
+      );
+      await videoService.updateStars(req.params, stars);
+      res.status(201).end();
+    } catch (err) {
+      next(err);
+    }
+  },
+  get: async (req, res, next) => {
+    try {
+      const reviews = await reviewService.findByItem(req.params);
+      res.status(200).json(reviews);
+    } catch (err) {
+      next(err);
+    }
+  },
+};
 
 export default reviewsController;
