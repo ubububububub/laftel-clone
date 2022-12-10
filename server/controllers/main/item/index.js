47630a98c0802f1f584ecd5a64a0b93e32fec24d
Salmambo <zxcvsqwe@gmail.com>
2022-12-10 17:49:58 +0900

    docs: GET /api/main/item/:itemId에서 episodes 기능 제거

--- server/controllers/main/item/index.js
+++ server/controllers/main/item/index.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { itemService, videoService, episodeService } from "../../../services";
+import { itemService, videoService } from "../../../services";
 import episodesController from "./episodes";
 import reviewsController from "./reviews";
 import relatedController from "./related";
@@ -11,7 +11,6 @@ itemController.get("/:itemId", async (req, res, next) => {
   try {
     const item = await itemService.getForDetail(req.params);
     const video = await videoService.getForDetail(item);
-    const episodes = await episodeService.findByItem(req.params);
     res.status(200).json({ video, episodes });
   } catch (err) {
     next(err);
