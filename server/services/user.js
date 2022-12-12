c41fa5cb0d02ddd53948f32efe79b18c03319140
Salmambo <zxcvsqwe@gmail.com>
2022-12-12 22:46:32 +0900

    feat: 회원가입시 이메일 인증

--- server/services/user.js
+++ server/services/user.js
@@ -1,16 +1,28 @@
 import { userModel } from "../models";
-import { hashPassword, sendMail, jwt } from "../utils";
+import { hashPassword, sendMail, jwt, redis } from "../utils";
 
 class UserService {
   constructor(userModel) {
     this.userModel = userModel;
   }
 
-  //   async checkEmail({ email }) {
-  //     const user = await this.userModel.findByEmail(email);
-  //     return user ? false : true;
-  //   }
+  async checkEmail({ email }) {
+    const user = await this.findByEmail(email);
+    if (user) return false;
+    const number = randomNumber();
+    const result = await redis.set(email, number);
+    await sendMail(email, `다음 인증번호를 입력해주십시오. >> ${number}`);
+    return result;
+  }
+  async checkNumber({ email, number }) {
+    const correct = await redis.get(email);
+    if (number != correct) throw new Error("bad request");
+    await redis.set(email, "certified");
+  }
   async join({ email, password, isLaftel, isInfo, isEvent }) {
+    const result = await redis.get(email);
+    if (result !== "certified") throw new Error("forbidden");
+    await redis.del(email);
     const hashed = await hashPassword.hash(password);
     const user = await this.userModel.create({
       email,
@@ -19,13 +31,13 @@ class UserService {
       isInfo,
       isEvent,
     });
-    await sendMail(user.email);
+    await sendMail(user.email, "라프텔에 가입되었습니다.");
   }
   async login({ email, password }) {
     const user = await this.findByEmail(email);
-    if (!user) throw new Error("login failed");
+    if (!user) throw new Error("bad request");
     const result = await hashPassword.compare(password, user.password);
-    if (!result) throw new Error("login failed");
+    if (!result) throw new Error("bad request");
     const accesstoken = await jwt.create(
       { email: user.email, role: user.role },
       "1h"
@@ -43,3 +55,9 @@ class UserService {
 const userService = new UserService(userModel);
 
 export default userService;
+
+function randomNumber() {
+  const number = Math.floor(Math.random() * 1000000);
+  if (number === 0) return randomNumber();
+  return String(number).padStart(6, "0");
+}
--- server/utils/index.js
+++ server/utils/index.js
@@ -1,5 +1,6 @@
 import hashPassword from "./hashPassword";
 import sendMail from "./sendMail";
 import jwt from "./jwt";
+import { redisClient, redis } from "./redis";
 
-export { hashPassword, sendMail, jwt };
+export { hashPassword, sendMail, jwt, redisClient, redis };
