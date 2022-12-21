6049c48aa81f7c8e81fbe7b2a7c265b10156061f
Salmambo <zxcvsqwe@gmail.com>
2022-12-21 23:08:18 +0900

    fix: 사용자평 api

--- server/controllers/main/item/reviews.js
+++ server/controllers/main/item/reviews.js
@@ -3,20 +3,21 @@ import { reviewService, videoService, starService } from "../../../services";
 const reviewsController = {
   post: async (req, res, next) => {
     const { content, star } = req.body;
-    let reviewAmount = 0,
-      stars = 0;
+    const videoInfo = {};
     try {
       if (content) {
-        reviewAmount = await reviewService.create(
+        const reviewAmount = await reviewService.create(
           req.params,
           req.headers,
           content
         );
+        if (reviewAmount) videoInfo.reviewAmount = reviewAmount;
       }
       if (star) {
-        stars = await starService.create(req.params, req.headers, star);
+        const stars = await starService.create(req.params, req.headers, star);
+        if (stars) videoInfo.stars = stars;
       }
-      await videoService.updateStars(req.params, { reviewAmount, stars });
+      await videoService.updateStars(req.params, videoInfo);
       res.status(201).end();
     } catch (err) {
       next(err);
@@ -25,8 +26,12 @@ const reviewsController = {
   get: async (req, res, next) => {
     try {
       const reviews = await reviewService.findByItem(req.params, req.headers);
-      const stars = await starService.findByItem(req.params, req.headers);
-      res.status(200).json({ reviews, stars });
+      const reviewsWithStar = await starService.findByItem(
+        req.params,
+        req.headers,
+        reviews
+      );
+      res.status(200).json(reviewsWithStar);
     } catch (err) {
       next(err);
     }
@@ -36,8 +41,8 @@ const reviewsController = {
     try {
       if (content) await reviewService.modify(req.params, req.headers, content);
       if (star) {
-        const result = await starService.modify(req.params, req.headers, star);
-        await videoService.updateStars(req.params, { star });
+        const stars = await starService.modify(req.params, req.headers, star);
+        await videoService.updateStars(req.params, { stars });
       }
       res.status(200).end();
     } catch (err) {
